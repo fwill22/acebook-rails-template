775cd782f056696a696f6f770ec9e149346091a7
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-07-29 17:22:13 +0100

    User model - calidations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ApplicationRecord
+  before_save { |user| user.email = email.downcase }
+  # before_save :create_remember_token
+  has_one_attached :avatar
+  has_many :posts
+  # has_many :comments
+  # has_many :likes
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+  format: { with: VALID_EMAIL_REGEX }, 
+  uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :confirm_password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  # DEFAULT_AVATAR = "default_avatar.png" -- ADD IMAGE FILE TO ASSETS FOLDER
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "posts#index"
+  
   resources :users do
   get '/signup', to: "users#index"
   # post '/signup', to: "users#create"
