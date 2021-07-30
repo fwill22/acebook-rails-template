781c02c7f72f63085441a93a98d009ddbded0ff6
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-07-30 12:45:56 +0100

    user controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+
+  skip_before_action :require_login, only: [:new, :create]
  
   def show
     @user = User.find(params[:id])
+    # @posts = @users.posts.order(created_at: :desc)
   end
 
   def new
@@ -13,15 +16,34 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Account Created"
-      redirect_to '/'
     else
+      flash[:error] = "Please create an account"
       render 'users#index'
     end
   end
 
+  def update
+    if current_user.authenticate(user_params[:password])
+      current_user.update(user_params)
+      log_in current_user
+        flash[:success] = 'Profile has been updated'
+        redirect_to "/users/#{current_user.id}"
+    else
+      redirect_to "/users/#{current_user.id}/edit"
+  end
+
+  # def user_posts
+  #   @posts = current_user.posts.order(created_at: :desc)
+  # end
+
+
   private
   def user_params
-    params.require(:user).permit(:name, :email, :dob, :password, :confirm_password)
+    params.require(:user).permit(:name,
+                                 :email,
+                                 :dob,
+                                 :password,
+                                 :confirm_password)
   end
 
   def check_db
